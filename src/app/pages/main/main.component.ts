db6b8546546dd22658fd1a3c7581fff787db0b61
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-01-22 20:34:02 +0100

    chore: display percentage change in disposable income #2

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -13,6 +13,10 @@ import {
   MatSelect,
   MatSelectChange,
 } from '@angular/material/select';
+import {
+  BarChartComponent,
+  Dataset,
+} from '../../components/bar-chart/bar-chart.component';
 
 /**
  * Represents an income group
@@ -25,11 +29,28 @@ export type IncomeGroup = {
 };
 
 /**
+ * Represents a political party
+ */
+export type Party = {
+  /** Color */
+  color: string;
+  /** Relative changes in income based on income groups */
+  changesIncomeGroups: number[];
+};
+
+/**
  * Displays main component
  */
 @Component({
   selector: 'app-main',
-  imports: [TranslocoModule, MatFormField, MatLabel, MatSelect, MatOption],
+  imports: [
+    TranslocoModule,
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    MatOption,
+    BarChartComponent,
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   standalone: true,
@@ -63,6 +84,74 @@ export class MainComponent implements OnInit {
     { index: 9, text: '250.001 - 2.000.000' },
   ];
 
+  /** Data of parties */
+  data = new Map<string, Party>([
+    [
+      'CDU',
+      {
+        color: 'rgb(0, 0, 0)',
+        changesIncomeGroups: [0.1, 0.1, 0.3, 0.6, 1.1, 1.8, 2.4, 3.2, 4.4, 5.1],
+      },
+    ],
+    [
+      'SPD',
+      {
+        color: 'rgb(227, 0, 15)',
+        changesIncomeGroups: [
+          1.9, 2.4, 3.1, 2.8, 2.5, 2.6, 2.3, 1.7, 1.0, -3.4,
+        ],
+      },
+    ],
+    [
+      'Grüne',
+      {
+        color: 'rgb(0, 152, 68)',
+        changesIncomeGroups: [
+          0.9, 2.8, 3.9, 3.6, 3.1, 2.1, 1.4, 0.7, -0.1, -3.8,
+        ],
+      },
+    ],
+    [
+      'FDP',
+      {
+        color: 'rgb(255, 204, 0)',
+        changesIncomeGroups: [
+          -2.1, -0.2, 1.4, 2.3, 3.7, 5.5, 6.8, 8.2, 9.8, 8.1,
+        ],
+      },
+    ],
+    [
+      'LINKE',
+      {
+        color: 'rgb(197, 30, 58)',
+        changesIncomeGroups: [
+          29.7, 12.4, 8.5, 6.4, 6.4, 6.7, 5.5, 2.7, -3.0, -27.0,
+        ],
+      },
+    ],
+    [
+      'AfD',
+      {
+        color: 'rgb(0, 152, 215)',
+        changesIncomeGroups: [0.0, 0.2, 1.1, 1.7, 2.8, 4.9, 6.1, 6.7, 7.7, 7.7],
+      },
+    ],
+    [
+      'BSW',
+      {
+        color: 'rgb(255, 165, 0)',
+        changesIncomeGroups: [
+          0.5, 1.4, 3.0, 2.8, 2.9, 3.0, 2.3, 1.3, 0.1, -2.2,
+        ],
+      },
+    ],
+  ]);
+
+  /** Datasets for selected income group */
+  incomeGroupDatasets: Dataset[] = [];
+  /** Labels for selected income group */
+  incomeGroupLabels: string[] = [];
+
   //
   // Constants
   //
@@ -78,8 +167,9 @@ export class MainComponent implements OnInit {
    * Handles on-init lifecycle phase
    */
   ngOnInit() {
-    // Handle query parameters
     this.handleQueryParameters();
+    this.initializeIncomeGroupLabels(this.data);
+    this.initializeIncomeGroupDatasets(this.data, -1);
   }
 
   /**
@@ -93,6 +183,52 @@ export class MainComponent implements OnInit {
   }
 
   //
+  // Initialization
+  //
+
+  /**
+   * Initialize labels for income group
+   * @param data data
+   * @private
+   */
+  private initializeIncomeGroupLabels(data: Map<string, Party>) {
+    this.incomeGroupLabels = Array.from(data.keys());
+  }
+
+  /** Initializes datasets for income group
+   *
+   * @param data data
+   * @param selectedIncomeGroupIndex
+   * @private
+   */
+  private initializeIncomeGroupDatasets(
+    data: Map<string, Party>,
+    selectedIncomeGroupIndex: number,
+  ) {
+    this.incomeGroupDatasets = [
+      {
+        axis: 'y',
+        label:
+          selectedIncomeGroupIndex != -1
+            ? this.translocoService.translate(
+                'pages.main.labels.percentage-income-change',
+                {},
+                this.lang,
+              )
+            : '',
+        data: Array.from(data.values()).map((party) =>
+          selectedIncomeGroupIndex != -1
+            ? party.changesIncomeGroups[selectedIncomeGroupIndex]
+            : 0,
+        ),
+        backgroundColor: Array.from(data.values()).map((party) => party.color),
+        borderColor: Array.from(data.values()).map((_) => 'transparent'),
+        borderWidth: 1,
+      },
+    ];
+  }
+
+  //
   // Actions
   //
 
@@ -100,5 +236,7 @@ export class MainComponent implements OnInit {
    * Handles change of income group
    * @param event event
    */
-  onIncomeGroupChanged(event: MatSelectChange) {}
+  onIncomeGroupChanged(event: MatSelectChange) {
+    this.initializeIncomeGroupDatasets(this.data, event.value);
+  }
 }
